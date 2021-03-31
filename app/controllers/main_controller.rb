ba14fb85d68e48570157d4049d1ecaf0fbf4f0fe
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-03-31 16:25:54 -0700

    flashing messages on controllers

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
     def index
+        #flash message
+        #flash.now[:notice] = "Logged in successfully"
+        #flash.now[:alert] = "Invalid email or account"
+        #print them out in the views so that the user can see them
     end
 end
