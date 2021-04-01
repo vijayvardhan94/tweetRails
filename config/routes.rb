57190ee40846498ea75c47adb0e260fc5262b5dd
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-04-01 12:43:46 -0700

    logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
   root  to: "main#index"
 end
