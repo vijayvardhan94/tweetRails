022210644717f7286f1710634a5891bddd691da4
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-04-01 11:42:56 -0700

    handling sign in errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,23 @@ class RegistrationsController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to root_path, notice: "Account created"
+        else
+            #flash[:alert] = "Something went wrong"
+            render :new
+        end
+    end
+
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
