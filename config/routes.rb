9e081448ef28f4b5f0913b8aba55663d6574fc97
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-04-01 12:59:36 -0700

    login form and validations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
   root  to: "main#index"
 end
