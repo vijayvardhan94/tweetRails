5ddb5cc2f34ad2418e48ccb366d35d964d8daa89
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-04-01 12:18:42 -0700

    user sign in with session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,5 +4,8 @@ class MainController < ApplicationController
         #flash.now[:notice] = "Logged in successfully"
         #flash.now[:alert] = "Invalid email or account"
         #print them out in the views so that the user can see them
+        if session[:user_id]
+            @user = User.find_by(id: session[:user_id])
+        end
     end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,7 @@ class RegistrationsController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Account created"
         else
             #flash[:alert] = "Something went wrong"
