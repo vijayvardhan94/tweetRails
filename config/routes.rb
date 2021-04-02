d4418e749f6d3efb735fa986a842eb7fbbd72b56
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-04-02 12:59:36 -0700

    controller for passwords, displaying current user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
   get "about-us", to: "about#index", as: :about
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
